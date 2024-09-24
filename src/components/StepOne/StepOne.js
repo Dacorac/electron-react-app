c17f76820893d5b3caa50a3af4b2e5418c8c2627
Daniela Cordoba <dacorac43@gmail.com>
2024-09-24 20:17:40 +1000

    Adding assets and creating page for Step one (selection of background)

---
+++ src/components/StepOne/StepOne.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState, useContext } from "react";
+import SelectNewBackground from "../../actions/Actions";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../../Store/Store";
+
+import "./StepOne.css";
+
+const StepOne = () => {
+  const [state, dispatch] = useContext(Context);
+  const [backgroundSelected, setBackgroundSelected] = useState(null);
+
+  const navigate = useNavigate();
+  
+  const backgroundList = [
+    { id: 1, url: "./assets/background_01.png" },
+    { id: 2, url: "./assets/background_02.png" },
+    { id: 3, url: "./assets/background_02.png" },
+  ];
+
+  useEffect(() => {
+    dispatch(SelectNewBackground(backgroundSelected));
+  }, [backgroundSelected])
+
+  return (  
+    <div className="step_one_container">
+      <div className="step_one_images_grid">
+        {backgroundList.map((background) => (
+          <div key={background.id} className="step_one_image">
+            <img 
+              className="step_one_image_button"
+              src={background.url} 
+              alt={background.id} 
+              onClick={() => setBackgroundSelected(background.id)}
+            />
+          </div>
+        ))} 
+      </div>
+      <div className="btn-container">
+        <button onClick={() => navigate("/step-two")}>Next</button>
+      </div>
+    </div>    
+  );
+}
+ 
+export default StepOne;
