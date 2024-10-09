83c0afa6a38803ad7a270c8d0ae68bf74b1643f9
Daniela Cordoba <dacorac43@gmail.com>
2024-10-09 17:50:08 +1000

    Fixing header size, header icon size and navigation to landing page on icon click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,16 +2,22 @@ import React from "react";
 import { FaHome } from "react-icons/fa";
 
 import "./Header.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const headerImgUrl = './assets/header_logo.png';
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate('/landing');
+  }
 
   return ( 
     <div className="header-container">
       <div className="header-image">
         <img src={headerImgUrl} alt="header image" />
       </div>
-      <div className="header-home-icon">
+      <div className="header-home-icon" onClick={goHome}>
         <FaHome />
       </div>
     </div>
